db1b55c825b9595925de0190e6c22ac37f08c787
asuralhp <asuralhp@gmail.com>
2022-03-09 20:48:35 +0800

    qwe

---
+++ Bash/file_system.sh
@@ -0,0 +1,17 @@
+which Command
+cd Target_Directory
+cd ..
+pwd
+ls
+
+touch File.ext
+mkdir Folder
+
+rm File.ext
+rmdir Folder
+rm -r Folder
+
+cp File.ext File_Copy.ext
+cp File.ext Target_Directory
+cp File.ext File_Copy.ext Target_Directory
+cp -r Folder Folder_copy # if Folder_copy exist Folder will move into it
