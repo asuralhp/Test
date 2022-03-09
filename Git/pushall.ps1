a0d83b71c8c9939187efd222febb6af55b6da327
asuralhp <asuralhp@gmail.com>
2022-03-09 17:52:44 +0800

    a

--- Git/pushall.ps1
+++ Git/pushall.ps1
@@ -1,3 +1,16 @@
+git init
+git clone url
+
+git remote add origin git@github.com:User/UserRepo.git
+git remote set-url origin git@github.com:User/AnotherUserRepo.git
+git remote -v
+
+git config --global user.email "asuralhp@gmail.com"
+git config --global user.name "asuralhp"
+git config --global --list
+git config --list
+
 git add -A
 git commit -m "Commit Message"
 git push
+
